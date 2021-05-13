b04eaadb906063ddd51cb1ba6308f6c6dd708176
Tocutoeltuco <tocutoeltuco@gmail.com>
2021-05-13 19:42:45 -0300

    Increase wait-for-it timeout
    The current timeout (15 seconds) is not enough for the DB to initialize.

--- updater/start.sh
+++ updater/start.sh
@@ -4,8 +4,8 @@
 set -e
 
 # Wait for databases to be available
-/wait-for-it.sh "$A801_IP:3306"
-/wait-for-it.sh "$DB_IP:3306"
+/wait-for-it.sh "$A801_IP:3306" --timeout=60
+/wait-for-it.sh "$DB_IP:3306" --timeout=60
 
 # Execute the command
 exec $@

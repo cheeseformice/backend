4bc61c1986a83f69eb37101424f1758b58f13916
Iván Gabriel <tocutoeltuco@gmail.com>
2021-08-27 20:34:18 -0300

    Enable auth service again

--- router/index.js
+++ router/index.js
@@ -107,7 +107,7 @@ let modules = [
 	// [hasPrefix, moduleName],
 	[true, "dressroom"],
 	[false, "healthcheck"],
-	// [false, "login"],
+	[false, "login"],
 	[false, "lookup"],
 	[false, "profile"],
 	[false, "session"],

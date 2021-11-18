1ae6629edfa31a064014c78b031ceff6338bdbde
Iván Gabriel <tocutoeltuco@gmail.com>
2021-11-18 17:39:07 -0300

    Update tfm language list

--- router/src/tfm.js
+++ router/src/tfm.js
@@ -12,14 +12,27 @@ const router = express.Router();
 
 const CACHE_EXPIRES = 60 * 60 * 1000; // 1 hour (ms)
 const languages = [
-	"ab", "af", "am", "ar", "av", "az", "be", "bh", "bm", "bo", "bs", "ca",
-	"ch", "co", "cs", "cv", "da", "dv", "ee", "en", "es", "eu", "ff", "fj",
-	"fr", "ga", "gl", "gu", "ha", "hi", "hr", "hu", "hz", "id", "ig", "ik",
-	"is", "iu", "jv", "kg", "kj", "kl", "kn", "kr", "ku", "kw", "la", "lg",
-	"ln", "lt", "lv", "mh", "mk", "mn", "mr", "mt", "na", "ne", "nl", "nr",
-	"ny", "oj", "or", "pa", "pl", "pt", "rc", "rn", "ru", "sa", "sd", "sg",
-	"si", "sl", "sn", "sq", "ss", "su", "sw", "te", "th", "tk", "tn", "tr",
-	"tt", "ty", "uk", "uz", "vi", "wa", "xh", "yo", "zh",
+	"ad", "ae", "af", "ag", "ai", "al", "am", "an", "ao", "ar", "as", "at",
+	"au", "aw", "ax", "az", "ba", "bb", "bd", "be", "bf", "bg", "bh", "bi",
+	"bj", "bm", "bn", "bo", "br", "bs", "bt", "bv", "bw", "by", "bz", "ca",
+	"cc", "cd", "cf", "cg", "ch", "ci", "ck", "cl", "cm", "cn", "co", "cr",
+	"cs", "cu", "cv", "cx", "cy", "cz", "de", "dj", "dk", "dm", "do", "dz",
+	"ec", "ee", "eg", "eh", "er", "es", "et", "fi", "fj", "fk", "fm", "fo",
+	"fr", "ga", "gb", "gd", "ge", "gf", "gh", "gi", "gl", "gm", "gn", "gp",
+	"gq", "gr", "gs", "gt", "gu", "gw", "gy", "hk", "hm", "hn", "hr", "ht",
+	"hu", "id", "ie", "il", "in", "io", "iq", "ir", "is", "it", "jm", "jo",
+	"jp", "ke", "kg", "kh", "ki", "km", "kn", "kp", "kr", "kw", "ky", "kz",
+	"la", "lb", "lc", "li", "lk", "lr", "ls", "lt", "lu", "lv", "ly", "ma",
+	"mc", "md", "me", "mg", "mh", "mk", "ml", "mm", "mn", "mo", "mp", "mq",
+	"mr", "ms", "mt", "mu", "mv", "mw", "mx", "my", "mz", "na", "nc", "ne",
+	"nf", "ng", "ni", "nl", "no", "np", "nr", "nu", "nz", "om", "pa", "pe",
+	"pf", "pg", "ph", "pk", "pl", "pm", "pn", "pr", "ps", "pt", "pw", "py",
+	"qa", "re", "ro", "rs", "ru", "rw", "sa", "sb", "sc", "sd", "se", "sg",
+	"sh", "si", "sj", "sk", "sl", "sm", "sn", "so", "sr", "st", "sv", "sy",
+	"sz", "tc", "td", "tf", "tg", "th", "tj", "tk", "tl", "tm", "tn", "to",
+	"tr", "tt", "tv", "tw", "tz", "ua", "ug", "um", "us", "uy", "uz", "va",
+	"vc", "ve", "vg", "vi", "vk", "vn", "vu", "wf", "ws", "xx", "ye", "yt",
+	"za", "zm", "zw",
 ];
 
 var translations = {};

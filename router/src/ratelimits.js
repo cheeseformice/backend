a67b0fde1e19f45581793d4511f4b88d2d693608
Iván Gabriel <tocutoeltuco@gmail.com>
2021-12-17 20:08:27 -0300

    Properly read IP for rate limits

--- router/src/ratelimits.js
+++ router/src/ratelimits.js
@@ -30,9 +30,8 @@ const checkRateLimit = (req, res, bucketName, param) => {
     }
 
     if (bucket.paramType === "ip") {
-      const { x_real_ip } = req.headers;
-      console.log(req.headers);
-      param = x_real_ip;
+      param = req.headers["x-real-ip"];
+      console.log(param)
     } else if (bucket.paramType === "user") {
       const user = checkAuthorization(req, res);
       if (!user) {

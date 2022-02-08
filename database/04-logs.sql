bab392167ac661d66bcf25b5071ea314445195a3
Iván Gabriel <tocutoeltuco@gmail.com>
2022-02-08 17:21:07 -0300

    add type to last_log tribe

--- database/04-logs.sql
+++ database/04-logs.sql
@@ -48,7 +48,7 @@ ALTER TABLE `member_changelog`
   ADD KEY (`id_member`);
 
 CREATE TABLE `last_log` (
-  `tribe` NOT NULL,
+  `tribe` boolean NOT NULL,
   `id` bigint(20) NOT NULL,
   `day` int(11),
   `week` int(11),
